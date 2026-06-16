feb926248c2700bdbb1d20826577c0ad22577fcf
Nathaniel Mosco <natemosco@gmail.com>
2026-06-15 23:20:44 -0400

    sync company-launches submission 2026-06-16T03:20:14.194Z

---
+++ DSA-problems/company-launches/2026-06-16-03-20-14-submission-b509e0d7.py
@@ -0,0 +1,4 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+# Wrong Answer: Submission produced output that did not match the expected output.
+def company_launches(launches, ads):
+  return [2, 5, 8]
